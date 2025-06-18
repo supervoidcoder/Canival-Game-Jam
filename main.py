425691c6eb86d45a1dbc94c64dc7b978b961a05c
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2025-06-18 17:40:44 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Ismael Chavez and Josh Nguyen
+#June 18, 2025
+# Carnival Game Jam
+
+''' All of our files are stored safely in this Codespace in the cloud,
+ and it won't get deleted unless github decides to or we don't use it for 30 days
+ which is more than enough. But still, we should commit our changes every once in a while
+ by doing 'git push' which creates a new save'''
+ 
+ 
