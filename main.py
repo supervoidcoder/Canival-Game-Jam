27a4b8141d825f17596fe2243725d3b5990f7665
supervoidcoder <voidcoder@outlook.com>
2025-06-18 14:13:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Ismael Chavez and Josh Nguyen
+# June 18 2025
+# Carnival Game Jam
+
+import pygame as pg
+import sys
+
+DISPLAYSURF = pg.display.set_mode((1000, 600))
+
+
+
+# we don't know EXACTLY what the game will be about so just put ideas in the chat
+
+#Setup
+#Global Constants
+
